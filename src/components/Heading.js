4fdf627e8744f58fb9c33e48ecea2fab6bff15eb
DESKTOP-4P1BDH9\SMICC PVT LTD <oturkar.divya@gmail.com>
2023-05-05 19:15:04 -0400

    first commit

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+       // return JST code that will get transformed in HTML code by react 
+       <h1>Weather app</h1>
+    )
+
+}
+
+export default Heading;
